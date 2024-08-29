8a69703296477897c44f808b10ae347e9e72bdaa
soumyabrata <soumyabratam6@gmail.com>
2024-08-29 21:19:11 +0530

    my first commit fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Form from "./components/Form";
 import MessageList from "./components/MessageList";
-import "./app.css";
+import "./App.css";
 const App = () => {
   return (
     <div className="App">
